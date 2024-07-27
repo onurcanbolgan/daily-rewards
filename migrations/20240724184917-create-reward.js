7150e189bdee995ee18163b73b1bf32041345660
onurcanbolgan <onurcanbolgan@gmail.com>
2024-07-27 13:29:58 +0300

    Readme changed

---
+++ migrations/20240724184917-create-reward.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rewards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      day: {
+        type: Sequelize.INTEGER
+      },
+      state: {
+        type: Sequelize.INTEGER
+      },
+      claimStartDate: {
+        type: Sequelize.DATE
+      },
+      claimEndDate: {
+        type: Sequelize.DATE
+      },
+      coin: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rewards');
+  }
+};
